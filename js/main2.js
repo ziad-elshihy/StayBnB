9e3ace339c1d6d47ad8fc7b39cca7c7bba8520ad
ziad-elshihy <elshihyziad@gmail.com>
2023-05-09 03:17:54 +0300

    added time - email

--- js/main2.js
+++ js/main2.js
@@ -19,3 +19,52 @@ icon.onclick = function() {
       icon.src = "images/moon.png";
    }
 }
+
+// array of items 
+
+const items = [{
+   id: 0,
+   title: "Bag 1",
+   price: 15,
+   img: "img/main7.jpg",
+   amount: 1
+},
+{
+   id: 1,
+   title: "Bag 2",
+   price: 25,
+   img: "img/main8.jpg",
+   amount: 1
+},
+{
+   id: 2,
+   title: "Bag 3",
+   price: 10,
+   img: "img/main9.jpg",
+   amount: 1
+},
+{
+   id: 3,
+   title: "Bag 4",
+   price: 30,
+   img: "img/main10.jpg",
+   amount: 1
+},
+{
+   id: 4,
+   title: "Bag 5",
+   price: 100,
+   img: "img/main11.jpg",
+   amount: 1
+}]
+
+let parentBoxs = document.querySelector('.parent-boxs')
+let currentItem = ""
+
+function rednerItems() {
+   items.forEach(item => {
+      currentItem += ``
+      parentBoxs.innerHTML = currentItem
+   })
+}
+rednerItems()
