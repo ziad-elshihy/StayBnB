42a0ee8da2f773f9d86ac2294a9128b290af4fd6
ziad-elshihy <elshihyziad@gmail.com>
2023-05-12 02:44:36 +0300

    fixing email.js + changing border

--- js/main.js
+++ js/main.js
@@ -20,47 +20,45 @@ function sendMail() {
       phone: document.getElementById("phone").value,
       message: document.getElementById("message").value,
    };
-
+   let errors1 = document.getElementById("errors1");
+   let errors2 = document.getElementById("errors2");
+   let errors3 = document.getElementById("errors3");
+   let errors4 = document.getElementById("errors4");
+   let text = '';
    const serviceID = "service_jzc67tq";
    const templateID = "template_4stpdhw";
-
+   if (info.userName === "" || info.userName.length < 5) {
+      text = "Please Enter Valid Username (at least 5 characters).";
+      errors1.innerHTML = text;
+      return false;
+   }
+   errors1.innerHTML = "";
+   if (info.email === "" || info.email.indexOf("@") == -1) {
+      text = "Please Enter Valid Email";
+      errors2.innerHTML = text;
+      return false;
+   }
+   errors2.innerHTML = "";
+   if (isNaN(info.phone) || info.phone.length <= 11) {
+      text = "Please Enter Valid Phone";
+      errors3.innerHTML = text;
+      return false;
+   }
+   errors3.innerHTML = "";
+   if (info.message.length < 10) {
+      text = "Please Enter Valid Message (at least 10 characters).";
+      errors4.innerHTML = text;
+      return false;
+   }
+   else {
+      alert("Your message has been sent successfully!!");
+   }
    emailjs.send(serviceID, templateID, info)
       .then(res => {
          document.getElementById("userName").value = "";
          document.getElementById("email").value = "";
          document.getElementById("phone").value = "";
          document.getElementById("message").value = "";
-         let errors1 = document.getElementById("errors1");
-         let errors2 = document.getElementById("errors2");
-         let errors3 = document.getElementById("errors3");
-         let errors4 = document.getElementById("errors4");
-         let text = '';
-         if (info.userName === "" || info.userName.length < 5) {
-            text = "Please Enter Valid Username";
-            errors1.innerHTML = text;
-            return false;
-         }
-         errors1.innerHTML = "";
-         if (info.email === "" || info.email.indexOf("@") == -1) {
-            text = "Please Enter Valid Email (at least 5 characters).";
-            errors2.innerHTML = text;
-            return false;
-         }
-         errors2.innerHTML = "";
-         if (isNaN(info.phone) || info.phone.length <= 11) {
-            text = "Please Enter Valid Phone";
-            errors3.innerHTML = text;
-            return false;
-         }
-         errors3.innerHTML = "";
-         if (info.message.length < 10) {
-            text = "Please Enter Valid Message (at least 10 characters).";
-            errors4.innerHTML = text;
-            return false;
-         }
-         else {
-            alert("Your message has been sent successfully!!");
-         }
       })
       .catch(err => console.log(err));
 }
